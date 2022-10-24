ebcc4dd327d9b93a15d1e483611e7f57b54b86d3
Tsai Wenja <agon978@gmail.com>
2022-10-25 02:06:26 +0800

    Change to production data

--- assets/apps/events_model.js
+++ assets/apps/events_model.js
@@ -35,7 +35,7 @@ var eventsModel = createApp({
   created: function() {
     const $this = this;
     loadGoogleSheet(
-      '1X1M6lDN863sonYaP9HhW3JQWX2sl0gVRdbbPz0HeK68',
+      '1g22PqPGaLXL-3DJeSkGlN-YS-_6Bju8igvoq-kDrp8o',
       '活動',
       { '日期': 'publish_at', '類型': 'type', '標題': 'title', '首圖': 'cover', '內文': 'content' },
       function(data) { $this.events = data; }
